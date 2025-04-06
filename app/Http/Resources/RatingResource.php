a838c660c6c132972c2363e645a0e438f9c6e8f5
badrakbik1 <bader.akbik@wulooj.com>
2025-04-06 14:04:56 +0300

    fix

--- app/Http/Resources/RatingResource.php
+++ app/Http/Resources/RatingResource.php
@@ -15,7 +15,8 @@ class RatingResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'level' => ['id' => $this->level?->id, 'words' => $this->level?->words],
+            'sound_id' => $this->sound_id,
+            'level' => ['id' => $this->level?->id, 'name' => $this->level?->name, 'words' => $this->level?->words],
             'degree' => $this->degree,
             'success_attempts' => $this->success_attempts,
             'failure_attempts' => $this->failure_attempts,
