b5111060cee11b5486f2607eb751de46dfdf1181
badrakbik1 <bader.akbik@wulooj.com>
2025-04-20 14:55:32 +0300

    fix

--- app/Http/Resources/LevelResource.php
+++ app/Http/Resources/LevelResource.php
@@ -15,6 +15,7 @@ class LevelResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'completed_sounds_to_success' => $this->completed_sounds_to_success,
             'progress' => new LevelProgressResource($this->letterProgress),
